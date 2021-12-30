3638a239a39761eb6d470592bc64f3d5d408d94a
Zach <zachary.tippit@gmail.com>
2021-12-30 12:43:35 -0700

    Fixed social buttons in Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Stack, Typography, Link } from '@mui/material'
+import { Box, Stack, Typography, Link, IconButton } from '@mui/material'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -44,7 +44,33 @@ const Footer = () => {
           <Box alignSelf='center'>
             <p style={{color:'white'}}>©2021 Zach Tippit</p>
           </Box>
-          <Box alignSelf='center'>
+          <Box alignSelf='center' sx={{pt: 1}}>
+            <div className={classes.socialBtnContainer}>
+                <div className={classes.aboutSocialBtn}>
+                    <Link href="https://github.com/ZachTippit/" target="_blank" >
+                        <IconButton>
+                            <GitHubIcon style={{fontSize: 50}}/>
+                        </IconButton>
+                    </Link>
+                </div>
+                <div className={classes.aboutSocialBtn}>
+                    <Link href="https://www.linkedin.com/in/zachtippit/" target="_blank" >
+                        <IconButton>
+                            <LinkedInIcon style={{fontSize: 50}} color='primary'/>
+                        </IconButton>
+                    </Link>
+                </div>
+                <div className={classes.aboutSocialBtn}>
+                    <Link href="https://www.instagram.com/zach_tippit/" target="_blank" >
+                        <IconButton>
+                            <InstagramIcon style={{fontSize: 50}} color='secondary'/>
+                        </IconButton>
+                    </Link>
+                </div>
+            </div>
+          </Box>
+          </Box>
+          {/* <Box alignSelf='center'>
             <Link href="https://github.com/ZachTippit/" target="_blank" >
               <GitHubIcon/>
             </Link>
@@ -55,17 +81,40 @@ const Footer = () => {
               <InstagramIcon/>
             </Link>
           </Box>
-      </Box>
+       */}
 
       {/* Mobile Render */}
       <Stack sx={{ display: { sm: 'flex', md: 'none'}}} direction="column" justifyContent="center" alignItems="center">
         <Link href='/contact'>Contact</Link>
         <Typography variant='body2' style={{color:'white'}}>©2021 Zach Tippit</Typography>
-        <div>
-          {socials.map(social => (
+        <Box alignSelf='center' sx={{pt: 1}}>
+            <div className={classes.socialBtnContainer}>
+                <div className={classes.aboutSocialBtn}>
+                    <Link href="https://github.com/ZachTippit/" target="_blank" >
+                        <IconButton>
+                            <GitHubIcon style={{fontSize: 30}}/>
+                        </IconButton>
+                    </Link>
+                </div>
+                <div className={classes.aboutSocialBtn}>
+                    <Link href="https://www.linkedin.com/in/zachtippit/" target="_blank" >
+                        <IconButton>
+                            <LinkedInIcon style={{fontSize: 30}} color='primary'/>
+                        </IconButton>
+                    </Link>
+                </div>
+                <div className={classes.aboutSocialBtn}>
+                    <Link href="https://www.instagram.com/zach_tippit/" target="_blank" >
+                        <IconButton>
+                            <InstagramIcon style={{fontSize: 30}} color='secondary'/>
+                        </IconButton>
+                    </Link>
+                </div>
+            </div>
+          </Box>
+          {/* {socials.map(social => (
             <Link href={social.link}><Typography align='center' variant='body2'>{social.title}</Typography></Link>
-          ))}
-        </div>
+          ))} */}
       </Stack>
     </div>
   );
